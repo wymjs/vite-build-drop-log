8eb2fbd6f78add44b8ec1a7f2016931ff909622c
twjw <aaaawjwa@gmail.com>
2024-08-19 21:21:44 +0800

    chore: 搬運並新增文檔

---
+++ index.ts
@@ -0,0 +1,24 @@
+import type { Plugin } from 'vite'
+
+const PLUGIN_NAME = '@wymjs/vite-build-drop-log'
+const FULL_PLUGIN_NAME = `vite-plugin-${PLUGIN_NAME}`
+const CONSOLE_NAME = `[${PLUGIN_NAME}]`
+
+export function buildDroLog(): any {
+	const plugin: Plugin = {
+		name: FULL_PLUGIN_NAME,
+		enforce: 'pre',
+		config(_, { command }) {
+			if (command === 'build') {
+				console.log(`[LOG]${CONSOLE_NAME} 已開啟 console 清除功能`)
+				return {
+					esbuild: {
+						drop: ['console', 'debugger'],
+					},
+				}
+			}
+		},
+	}
+
+	return plugin
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+@wymjs/vite-build-drop-log
+===
+
+> 構建模式移除 console
+
+## 安裝
+
+```shell
+$ pnpm i -D @wymjs/vite-build-drop-log
+```
+
+## 使用
+
+```typescript
+import { defineConfig } from 'vite'
+import { buildDroLog } from '@wymjs/vite-build-drop-log'
+
+export default () => {
+  return defineConfig({
+    plugins: [
+      // 配置下去就會在 vite build 時移除項目中的 console 與 debugger
+      // 恩...就這樣
+      buildDroLog(),
+    ],
+  })
+}
+```
